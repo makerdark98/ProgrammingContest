c6d38b34dc1183a286988cef567b763be3e8140f
md98 <makerdark98@naver.com>
2017-08-29 22:58:16 +0900

    Clear 1202

--- boj/1202.cpp
+++ boj/1202.cpp
@@ -1,46 +1,36 @@
+#include <stdio.h>
 #include <iostream>
-#include <vector>
+#include <queue>
 #include <algorithm>
+#include <set>
+
 using namespace std;
-
-int main() {
-    vector< pair<int, int> > Jewelry;
-    int N, K, M, V, sum=0;
-    vector<int> C;
+int main(){
+    int N, K;
+    priority_queue<pair<int,int> >jew;
     scanf("%d %d", &N, &K);
-    for(int i =0;i<N;i++){
-        scanf("%d %d", &M, &V);
-        Jewelry.push_back(make_pair(M,V));
+    for(int i=0;i<N;i++){
+        int M, V;
+        scanf("%d %d",&M,&V);
+        jew.push(make_pair(V,M));
     }
+    multiset<int> bag;
+    long long sum=0;
     for(int i=0;i<K;i++){
-        int j;
-        scanf("%d", &j);
-        C.push_back(j);
+        int C;
+        scanf("%d", &C);
+        bag.insert(C);
     }
-
-    sort(Jewelry.begin(), Jewelry.end(), [](pair<int, int> x1, pair<int, int> x2) -> bool{
-        if(x1.second==x2.second) return x1.first<x2.first;
-        return x1.second>x2.second;
-    });
-    sort(C.begin(), C.end(), [](int x1, int x2) -> bool{
-        return x1<x2;
-    });
-
-    for(int i =0;i<N;i++){
-        printf("%d %d\n", Jewelry[i].first, Jewelry[i].second);
+    while(!jew.empty()){
+        int V = jew.top().first;
+        int M = jew.top().second;
+        jew.pop();
+        auto tmp = bag.lower_bound(M);
+        if (tmp == bag.end())
+            continue;
+        bag.erase(tmp);
+        sum += V;
     }
-    for(int i =0;i<K;i++){
-        printf("%d\n", C[i]);
-    }
-    for(int i=0, j;i<K;i++){
-        j=0;
-        if(C[j]>Jewelry[i].first) {
-            sum += Jewelry[i].second;
-            C.erase(C.begin() +j);
-        }
-    }
-    printf("%d", sum);
-
-
+    printf("%lld\n",sum);
     return 0;
 }
