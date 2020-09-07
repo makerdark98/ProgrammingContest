4ec71fd5e9a6869f78d8749e1db8490b164776cc
Min-Uk.Lee <makerdark98@gmail.com>
2020-09-07 23:59:48 +0900

    test cpplint

--- boj/1003.cpp
+++ boj/1003.cpp
@@ -1,20 +1,26 @@
+/**
+ * Copyright [2020] <makerdark98@gmail.com>"
+ */
 #include <iostream>
-using namespace std;
-int main(){
+using std::cout;
+int main() {
     int T;
-    scanf("%d",&T);
-    long long fibo[200];
-    fibo[0]=0;
-    fibo[1]=1;
-    for(int i=2;i<=41;i++){
-        fibo[i]=fibo[i-1]+fibo[i-2];
+    scanf("%d", &T);
+    int64_t fibo[200];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for (int i = 2; i <= 41; ++i) {
+        fibo[i] = fibo[i-1] + fibo[i-2];
     }
-    while(T--){
+    while (T--) {
         int n;
-        scanf("%d",&n);
-        if(n==0) printf("1 0\n");
-        else if(n==1) printf("0 1\n");
-        else printf("%lld %lld\n", fibo[n-1], fibo[n]);
+        scanf("%d", &n);
+        if (n == 0)
+          cout << "1 0\n";
+        else if (n == 1)
+          cout << "0 1\n";
+        else
+          cout << fibo[n-1] << " " << fibo[n] << "\n";
     }
     return 0;
 }
